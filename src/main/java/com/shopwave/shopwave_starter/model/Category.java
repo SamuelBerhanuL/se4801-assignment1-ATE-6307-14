7f0407ba60cf6cd8c38d7bc49568cea6d2dba58e
USER <samberhanuclassno67@gmail.com>
2026-04-03 09:44:43 +0300

    Added domain entities with full JPA mappings and Lombok annotations

---
+++ src/main/java/com/shopwave/shopwave_starter/model/Category.java
@@ -0,0 +1,21 @@
+package com.shopwave.shopwave_starter.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+}
---
+++ src/main/java/com/shopwave/shopwave_starter/model/Product.java
@@ -0,0 +1,34 @@
+package com.shopwave.shopwave_starter.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private Integer stock;
+
+    @ManyToOne
+    private Category category;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
